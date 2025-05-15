320c3ae4131707b13d1eb07b56d5438f2b1ce240
David B. <88238063+trainspotter-rhld@users.noreply.github.com>
2025-05-15 18:03:49 +0200

    ein bisschen abgeändert

--- EVP/Properties/Resources.Designer.cs
+++ EVP/Properties/Resources.Designer.cs
@@ -71,6 +71,16 @@ namespace EVP.Properties {
         }
         
         /// <summary>
+        ///   Sucht eine lokalisierte Ressource vom Typ System.Drawing.Icon ähnlich wie (Symbol).
+        /// </summary>
+        internal static System.Drawing.Icon EVPICO {
+            get {
+                object obj = ResourceManager.GetObject("EVPICO", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Sucht eine lokalisierte Ressource vom Typ System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap Logo {
